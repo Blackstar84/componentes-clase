9e73849852bd77d087e448de2d3a9a64848df0e5
Blackstar84 <carlosalmada84@gmail.com>
2023-04-10 17:25:29 -0400

    se agrega hasta la clase de trabajando con el estado

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import PropTypes from 'prop-types'
 class App extends React.Component{
+
     static defaultProps = {
         name: "Cody Default"
     }
+
     static propTypes = {
         name: PropTypes.string
     }
+
+    constructor(props){
+        super(props);
+        this.state = {
+            contador: 0
+        }
+    }
+
   render(){
     console.log(this.props);
-    return <p>Hola Mundo! </p>
+        return(
+            <div>
+                
+                <p>Contador: {this.state.contador} </p>
+                <button onClick={()=> this.setState({contador: this.state.contador + 1 }) }>Sumar</button>
+            </div>
+        ) 
+    
+    /* console.log(this.props);
+    return <p>Hola Mundo! </p> */
+    
   }
 }
 /* export default App; */
-render (<App name={2} />);
+//render (<App name={2} />);
 export default App;
