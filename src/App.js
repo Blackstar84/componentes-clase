43f06686ed5f49237d4ed765f805aab99ebd450c
Blackstar84 <carlosalmada84@gmail.com>
2023-04-10 15:02:57 -0400

    Se agrega la clase de trabajando con Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import { render } from '@testing-library/react';
+import PropTypes from 'prop-types'
 class App extends React.Component{
-  
+    static defaultProps = {
+        name: "Cody Default"
+    }
+    static propTypes = {
+        name: PropTypes.string
+    }
+  render(){
+    console.log(this.props);
+    return <p>Hola Mundo! </p>
+  }
 }
-
+/* export default App; */
+render (<App name={2} />);
 export default App;
